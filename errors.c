869a2f2415a101d1b3b384beab9d8fc247b6e9e8
Meriemamahry <112652236+Meriemamahry@users.noreply.github.com>
2023-05-24 20:56:47 +0200

    Update

--- errors.c
+++ errors.c
@@ -1,17 +1,12 @@
 #include "shell.h"
-
 /**
  *_eputs - prints an input string
- * @str: the string to be printed
- *
- * Return: Nothing
+ * @str: string
  */
 void _eputs(char *str)
 {
 	int i = 0;
 
-	if (!str)
-		return;
 	while (str[i] != '\0')
 	{
 		_eputchar(str[i]);
@@ -22,9 +17,7 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 1 or -1
  */
 int _eputchar(char c)
 {
@@ -40,14 +33,11 @@ int _eputchar(char c)
 		buf[i++] = c;
 	return (1);
 }
-
 /**
  * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @c: character
+ * @fd: filedescriptor
+ * Return: 1 or -1
  */
 int _putfd(char c, int fd)
 {
