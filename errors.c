ebde7f5c9e48569cc5786e9af221e08b861948e4
Meriemamahry <112652236+Meriemamahry@users.noreply.github.com>
2023-05-24 21:24:19 +0000

    Update errors.c

--- errors.c
+++ errors.c
@@ -3,83 +3,77 @@
 /**
  *_eputs - prints an input string
  * @str: the string to be printed
- *
  * Return: Nothing
  */
-void _eputs(char *str)
+void _eputs(char *str1)
 {
-	int i = 0;
+int k = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_eputchar(str[i]);
-		i++;
-	}
+if (!str1)
+return;
+while (str1[k] != '\0')
+{
+_eputchar(str1[k]);
+k++;
+}
 }
 
 /**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _eputchar - writes
+ * @c: The character
+ * Return: 1
  */
 int _eputchar(char c)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+static int k;
+static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+if (c == BUF_FLUSH || k >= WRITE_BUF_SIZE)
+{
+write(2, buf, k);
+k = 0;
+}
+if (c != BUF_FLUSH)
+buf[k++] = c;
+return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
+ * _putfd - writes
  * @c: The character to print
  * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 1
  */
 int _putfd(char c, int fd)
 {
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(fd, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
 }
 
 /**
- *_putsfd - prints an input string
- * @str: the string to be printed
+ *_putsfd - prints an
+ * @str: the string
  * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
+ * Return: the number
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+int i = 0;
 
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
-	}
-	return (i);
+if (!str)
+return (0);
+while (*str)
+{
+i += _putfd(*str++, fd);
+}
+return (i);
 }
