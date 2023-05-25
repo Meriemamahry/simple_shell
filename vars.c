327c53c3ffd077092a78f3a177fd1bbbd9c1fa1f
Meriemamahry <112652236+Meriemamahry@users.noreply.github.com>
2023-05-25 19:44:42 +0200

    Update vars.c

--- vars.c
+++ vars.c
@@ -111,13 +111,13 @@ continue;
 if (!_strcmp(info->argv[i], "$?"))
 {
 replace_string(&(info->argv[i]),
-_strdup(convert_number(info->status, 10, 0)));
+_strdup(convnum(info->status, 10, 0)));
 continue;
 }
 if (!_strcmp(info->argv[i], "$$"))
 {
 replace_string(&(info->argv[i]),
-_strdup(convert_number(getpid(), 10, 0)));
+_strdup(convnum(getpid(), 10, 0)));
 continue;
 }
 node = node_starts_with(info->env, &info->argv[i][1], '=');
