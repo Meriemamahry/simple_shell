5f059c10baeda19d816885c7db0a9d53272a3f87
artful01 <104211751+artful01@users.noreply.github.com>
2023-05-25 18:00:43 +0000

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -17,7 +17,7 @@ return (NULL);
 if (!d)
 d = " ";
 for (i = 0; str[i] != '\0'; i++)
-if (!delim(str[i], d) && (idelim(str[i + 1], d) || !str[i + 1]))
+if (!delim(str[i], d) && (delim(str[i + 1], d) || !str[i + 1]))
 numwords++;
 if (numwords == 0)
 return (NULL);
@@ -26,10 +26,10 @@ if (!s)
 return (NULL);
 for (i = 0, j = 0; j < numwords; j++)
 {
-while (is_delim(str[i], d))
+while (delim(str[i], d))
 i++;
 k = 0;
-while (!is_delim(str[i + k], d) && str[i + k])
+while (!delim(str[i + k], d) && str[i + k])
 k++;
 s[j] = malloc((k + 1) * sizeof(char));
 if (!s[j])
