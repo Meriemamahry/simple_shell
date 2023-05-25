7a27a6774f8c15b9dd4f774246b9606aa75fb54f
Meriemamahry <112652236+Meriemamahry@users.noreply.github.com>
2023-05-25 19:12:37 +0200

    environ.c

--- environ.c
+++ environ.c
@@ -41,7 +41,7 @@ int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number\n");
+		ptu("Incorrect number\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
