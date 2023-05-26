2cc3b75595c0c82a3c7d11a86bd99900b4ef9a31
artful01 <104211751+artful01@users.noreply.github.com>
2023-05-26 22:29:34 +0000

    Update vars.c

--- vars.c
+++ vars.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * is_chain - test chain delimiter
- * @info: struct
+ * washchaine - test chain delimiter
+ * @information: struct
  * @buf: the char buffer
  * @p: address of buf
  * Return: 1 or 0
  */
-int is_chain(info_t *info, char *buf, size_t *p)
+int washchaine(info_t *information, char *buf, size_t *p)
 {
 size_t j = *p;
 
@@ -15,18 +15,18 @@ if (buf[j] == '|' && buf[j + 1] == '|')
 {
 buf[j] = 0;
 j++;
-info->cmd_buf_type = CMD_OR;
+information->cmd_buf_type = CMD_OR;
 }
 else if (buf[j] == '&' && buf[j + 1] == '&')
 {
 buf[j] = 0;
 j++;
-info->cmd_buf_type = CMD_AND;
+information->cmd_buf_type = CMD_AND;
 }
-else if (buf[j] == ';') /* found end of this command */
+else if (buf[j] == ';') 
 {
 buf[j] = 0;
-info->cmd_buf_type = CMD_CHAIN;
+information->cmd_buf_type = CMD_CHAIN;
 }
 else
 return (0);
@@ -36,28 +36,28 @@ return (1);
 
 /**
  * check_chain - checks last status
- * @info: struct
+ * @information: struct
  * @buf: the char buffer
  * @p: address of current position
  * @i: starting position in buf
  * @len: length of buf
  * Return: Void
  */
-void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+void check_chain(info_t *information, char *buf, size_t *p, size_t i, size_t len)
 {
 size_t j = *p;
 
-if (info->cmd_buf_type == CMD_AND)
+if (information->cmd_buf_type == CMD_AND)
 {
-if (info->status)
+if (information->status)
 {
 buf[i] = 0;
 j = len;
 }
 }
-if (info->cmd_buf_type == CMD_OR)
+if (information->cmd_buf_type == CMD_OR)
 {
-if (!info->status)
+if (!information->status)
 {
 buf[i] = 0;
 j = len;
@@ -68,79 +68,79 @@ j = len;
 
 /**
  * replace_alias - tokenized string
- * @info: the parameter
+ * @information: the parameter
  * Return: 1 or 0
  */
-int replace_alias(info_t *info)
+int replace_alias(info_t *information)
 {
 int i;
-list_t *node;
+list_t *nodetqwd;
 char *p;
 
 for (i = 0; i < 10; i++)
 {
-node = node_starts_with(info->alias, info->argv[0], '=');
-if (!node)
+nodetqwd = nodetabda(information->alias, information->argv[0], '=');
+if (!nodetqwd)
 return (0);
-free(info->argv[0]);
-p = str_chr(node->str, '=');
+free(information->argv[0]);
+p = str_chr(nodetqwd->str, '=');
 if (!p)
 return (0);
 p = _strdup(p + 1);
 if (!p)
 return (0);
-info->argv[0] = p;
+information->argv[0] = p;
 }
 return (1);
 }
 
 /**
  * replace_vars - replaces vars
- * @info: struct
+ * @information: struct
  * Return: 1 or 0
  */
-int replace_vars(info_t *info)
+int replace_vars(info_t *information)
 {
 int i = 0;
-list_t *node;
+list_t *nodetqwd;
 
-for (i = 0; info->argv[i]; i++)
+for (i = 0; information->argv[i]; i++)
 {
-if (info->argv[i][0] != '$' || !info->argv[i][1])
+if (information->argv[i][0] != '$' || !information->argv[i][1])
 continue;
-if (!_strcmp(info->argv[i], "$?"))
+if (!_strcmp(information->argv[i], "$?"))
 {
-replace_string(&(info->argv[i]),
-_strdup(convnum(info->status, 10, 0)));
+replace_string(&(information->argv[i]),
+_strdup(convnum(information->status, 10, 0)));
 continue;
 }
-if (!_strcmp(info->argv[i], "$$"))
+if (!_strcmp(information->argv[i], "$$"))
 {
-replace_string(&(info->argv[i]),
+replace_string(&(information->argv[i]),
 _strdup(convnum(getpid(), 10, 0)));
 continue;
 }
-node = node_starts_with(info->env, &info->argv[i][1], '=');
-if (node)
+nodetqwd = nodetabda(information->env, &information->argv[i][1], '=');
+if (nodetqwd)
 {
-replace_string(&(info->argv[i]),
-_strdup(str_chr(node->str, '=') + 1));
+replace_string(&(information->argv[i]),
+_strdup(str_chr(nodetqwd->str, '=') + 1));
 continue;
 }
-replace_string(&info->argv[i], _strdup(""));
+replace_string(&information->argv[i], _strdup(""));
 }
 return (0);
 }
 
 /**
- * replace_string - replaces string in the code
- * @old: old string
- * @new: new string
+ * bdlstr - replaces string in the code
+ * @oldstr: old string
+ * @newstr: new string
  * Return: 1 or 0
  */
-int replace_string(char **old, char *new)
+int bdlstr(char **oldstr, char *newstr)
 {
-free(*old);
-*old = new;
+free(*oldstr);
+*oldstr = newstr;
 return (1);
 }
